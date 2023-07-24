4b13c044905a2db58e96c1c22ab58355374d9259
kaoutaraitmoulay <kaoutaraitmoulay@gmail.com>
2023-07-24 14:06:18 +0000

    task 0

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - _putchar alphabets
+ * @c: the number to be checked
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - aboslute value
+ * @n: the number to be checked
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	if (n < 0)
+	{
+		return (-1 * n);
+	}
+	else
+	{
+		return (0);
+	}
+}
